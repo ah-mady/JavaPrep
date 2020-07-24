2bd471a8fc27c43c948d25a48e3354fba7541526
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-07-24 23:11:30 +0200

    added tasks upto fizzbuzz

---
+++ week-02/day-02/src/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+    int count = 0;
+
+    while (count<100){
+      System.out.println("I won't cheat on the exam! ");
+      count++;
+    }
+  }
+}
