1995227b4d4c9ae43ce42f48a0adcb0a160115c6
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-07-24 23:16:02 +0200

    added draw triangle

---
+++ week-02/day-02/src/DrawTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+  public static void main(String[] args) {
+    // Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+    System.out.println("Please enter a number for triangle width: ");
+    Scanner scanner = new Scanner(System.in);
+    int numberOfStars = scanner.nextInt();
+    for (int i = 0; i <numberOfStars ; i++) {
+      for (int j = 0; j < i ; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+}
