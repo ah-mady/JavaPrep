8b68eab2a8ff24dd65cb169f26bcc9fd00c07da3
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-01 17:23:04 +0200

    adding TodoPrint task

---
+++ week-03/day-03/src/TodoPrint.java
@@ -0,0 +1,22 @@
+public class TodoPrint {
+  public static void main(String[] args) {
+    String todoText = " - Buy milk\n";
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected output:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    String myTodo = "My todo:\n";
+    todoText = myTodo  +todoText;
+    todoText = todoText.concat(" - Download games\n\t- Diablo");
+
+    System.out.println(todoText);
+
+  }
+}
