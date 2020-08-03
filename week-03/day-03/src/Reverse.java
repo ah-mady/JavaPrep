b91d2e189f5269738eb823276c572b1826b2ba05
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-03 14:04:09 +0200

    adding reversed task

---
+++ week-03/day-03/src/Reverse.java
@@ -0,0 +1,27 @@
+public class Reverse {
+  public static void main(String[] args) {
+    String toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a method that can reverse a String, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
+
+    System.out.println(reverse(toBeReversed));
+
+  }
+
+  private static char[] reverse(String toBeReversed) {
+    char[] reversedChar = toBeReversed.toCharArray();
+    int begin = 0;
+    int end = reversedChar.length -1;
+    char temp;
+    while (end > begin) {
+      temp = reversedChar[begin];
+      reversedChar[begin] = reversedChar[end];
+      reversedChar[end] = temp;
+      end--;
+      begin++;
+    }
+    return reversedChar;
+  }
+}
