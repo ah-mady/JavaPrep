3586c32efe85774e820888ea014d87f6b940ad92
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-03 21:23:32 +0200

    adding CenteredSquare task

---
+++ week-04/day-01/src/CenteredSquare.java
@@ -0,0 +1,42 @@
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CenteredSquare {
+  public static void mainDraw(Graphics graphics) {
+    // Draw a green 10x10 square to the canvas' center.
+
+    int size = 10;
+    graphics.setColor(Color.RED);
+    graphics.drawRect((WIDTH/2)-(size/2), (HEIGHT/2)-(size/2),size, size);
+
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
+
