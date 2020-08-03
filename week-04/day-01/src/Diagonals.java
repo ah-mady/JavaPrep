2aba2aee3ae043d733776298ea55446c34d817f5
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-03 20:31:06 +0200

    ColoredBox task ready - more practicle solution

--- week-04/day-01/src/Diagonals.java
+++ week-04/day-01/src/Diagonals.java
@@ -6,23 +6,14 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ColoredBox {
+public class Diagonals {
   public static void mainDraw(Graphics graphics) {
-    // Draw a box that has different colored lines on each edge.
+    // Draw the canvas' diagonals.
+    // If it starts from the upper-left corner it should be green, otherwise it should be red.
 
-    int size = 90;
+    graphics.setColor(Color.GREEN);
 
-    graphics.setColor(Color.BLACK);
-    graphics.drawLine(WIDTH/2 - size, HEIGHT/2-size, WIDTH/2-size, HEIGHT/2+size);
 
-    graphics.setColor(Color.BLUE);
-    graphics.drawLine(WIDTH/2 - size, HEIGHT/2-size, WIDTH/2+size, HEIGHT/2-size);
-
-    graphics.setColor(Color.RED);
-    graphics.drawLine(WIDTH/2 + size, HEIGHT/2-size, WIDTH/2+size, HEIGHT/2+size);
-
-    graphics.setColor(Color.MAGENTA);
-    graphics.drawLine(WIDTH/2 - size, HEIGHT/2+size, WIDTH/2+size, HEIGHT/2+size);
   }
 
   // Don't touch the code below
