5a28225b4e2c887610da6dbb9a0dff2b91388bf1
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-08 12:35:17 +0200

    formatted codes

--- week-04/day-03/src/Anagram.java
+++ week-04/day-03/src/Anagram.java
@@ -2,23 +2,20 @@ import java.util.Arrays;
 
 public class Anagram {
   public static void main(String[] args) {
-    System.out.println(isAnagram("green","eergn"));
+    isAnagram("green", "eergn");
   }
 
-  private static boolean isAnagram(String string1, String string2) {
-
+  private static void isAnagram(String string1, String string2) {
     boolean status = true;
-    if (string1.length()!=string2.length()){
+    if (string1.length() != string2.length()) {
       status = false;
     } else {
       char[] string1Array = string1.toLowerCase().toCharArray();
       char[] string2Array = string2.toLowerCase().toCharArray();
-
       Arrays.sort(string1Array);
       Arrays.sort(string2Array);
-      status = Arrays.equals(string1Array,string2Array);
+      status = Arrays.equals(string1Array, string2Array);
     }
-
-    return status;
+    System.out.println(status);
   }
 }
--- week-04/day-03/src/PalindromeBuilder.java
+++ week-04/day-03/src/PalindromeBuilder.java
@@ -5,10 +5,9 @@ public class PalindromeBuilder {
 
   private static void createPalindrome(String inputForPalindrome) {
     String finalPalindrome = inputForPalindrome;
-
     char[] inputCharArray = inputForPalindrome.toLowerCase().toCharArray();
     char[] copyOfInputArray = new char[inputCharArray.length];
-    for (int i = inputCharArray.length-1; i >= 0; i--) {
+    for (int i = inputCharArray.length - 1; i >= 0; i--) {
       for (int j = 0; j < inputCharArray.length; j++) {
         copyOfInputArray[j] += inputCharArray[i];
         i--;
