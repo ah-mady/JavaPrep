f75de0f06af36e44111b2eac31014a45f6c99fa4
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-08 12:33:06 +0200

    adding PalindromeBuilder task

--- week-04/day-03/src/PalindromeBuilder.java
+++ week-04/day-03/src/PalindromeBuilder.java
@@ -1,5 +1,22 @@
-public class PalindromBuilder {
+public class PalindromeBuilder {
   public static void main(String[] args) {
-    
+    createPalindrome("123");
+  }
+
+  private static void createPalindrome(String inputForPalindrome) {
+    String finalPalindrome = inputForPalindrome;
+
+    char[] inputCharArray = inputForPalindrome.toLowerCase().toCharArray();
+    char[] copyOfInputArray = new char[inputCharArray.length];
+    for (int i = inputCharArray.length-1; i >= 0; i--) {
+      for (int j = 0; j < inputCharArray.length; j++) {
+        copyOfInputArray[j] += inputCharArray[i];
+        i--;
+      }
+      break;
+    }
+    String reversedString = new String(copyOfInputArray);
+    finalPalindrome += reversedString;
+    System.out.println(finalPalindrome);
   }
 }
