c68fdc54edb08be275ac5776cb2ae623d0047eaf
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-08-29 12:02:37 +0200

    (fib) feat: added nano time counter

--- week-07/day-03/src/Fibonacci.java
+++ week-07/day-03/src/Fibonacci.java
@@ -1,15 +1,31 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.*;
 
 public class Fibonacci {
   // Fibonacci numbers: 1 1 2 3 5 8 13 21 34 55...
   // fib(10) --> should return with the 10. fibonacci number: 55
 
+  private static Map<Integer, Long> fibonacciNumbers = new HashMap<Integer, Long>(){{
+    put(1, 1L);
+    put(2, 1L);
+  }};
+
   public static void main(String[] args) {
-    LocalDateTime start = LocalDateTime.now();
+    //milli seconds
+    /*LocalDateTime start = LocalDateTime.now();
     System.out.println(fib(45));
     LocalDateTime end = LocalDateTime.now();
-    System.out.println(Duration.between(start, end).toMillis());
+    System.out.println(Duration.between(start, end).toMillis());*/
+
+    // nano seconds
+    long start = System.nanoTime();
+
+    System.out.println(fib(48));
+
+    long end = System.nanoTime();
+    double executionTimeInMillis = (end - start) / 1000.;
+    System.out.println(executionTimeInMillis);
   }
 
   public static long fib(int n) {
@@ -20,6 +36,23 @@ public class Fibonacci {
       return 1;
     }
     return fib(n - 1) + fib(n - 2);
+  }
+
+  public static long fibWithMemoization(int n) {
+    if (n < 1) {
+      return 0;
+    }
+    long prev = fibonacciNumbers.containsKey(n - 1) ? fibonacciNumbers.get(n - 1) : fibWithMemoization(n - 1);
+    long prevPrev = fibonacciNumbers.containsKey(n - 2) ? fibonacciNumbers.get(n - 2) : fibWithMemoization(n - 2);
+
+    if (!fibonacciNumbers.containsKey(n - 1)) {
+      fibonacciNumbers.put(n - 1, prev);
+    }
+    if (!fibonacciNumbers.containsKey(n - 2)) {
+      fibonacciNumbers.put(n - 2, prevPrev);
+    }
+
+    return prev + prevPrev;
   }
 
   public static long fibWithForLoop(int n) {
