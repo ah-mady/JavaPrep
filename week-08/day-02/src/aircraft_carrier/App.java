d14a6dc6d3b3ab7499012e97037754f012634905
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-09-03 13:53:49 +0200

    (aircraft_carrier) feat: refactored App class

--- week-08/day-02/src/aircraft_carrier/App.java
+++ week-08/day-02/src/aircraft_carrier/App.java
@@ -4,11 +4,12 @@ public class App {
   public static void main(String[] args) throws Exception {
     Aircraft jet1 = new F16();
     Aircraft jet2 = new F35();
+    Aircraft jet3 = new F35();
 
     //jet1.refill(100);
 
     Carrier carr = new Carrier(1000, 6000);
-    Carrier carr2 = new Carrier(2000,1000);
+    Carrier carr2 = new Carrier(2000, 1000);
 
 
     carr.add(jet1);
@@ -16,6 +17,7 @@ public class App {
 
     carr2.add(jet1);
     carr2.add(jet2);
+    carr2.add(jet3);
 
     carr.fill();
     carr2.fill();
@@ -29,8 +31,11 @@ public class App {
 
     //System.out.println(carr.aircraftList.get(0).getStatus());
     //System.out.println(carr.aircraftList.get(1).getStatus());
+    carr.fill();
+    carr2.fill();
     System.out.println(carr.status());
     System.out.println();
+
     System.out.println(carr2.status());
   }
 }
