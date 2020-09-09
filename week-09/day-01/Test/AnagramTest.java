96647d42291614e06f7ba33c624ada6fae55758d
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-09-09 17:27:18 +0200

    (Anagram) feat: added class and test functions

---
+++ week-09/day-01/Test/AnagramTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class AnagramTest {
+
+  Anagram anagram;
+  String[] words;
+
+  @BeforeEach
+  public void setWords(){
+    anagram = new Anagram();
+  }
+
+  @Test
+  void wordsAreAnagram() {
+    words = new String[] {"listen", "silent"};
+    assertTrue(true, String.valueOf(anagram.isAnagram(words)));
+  }
+
+  @Test
+  void withOnlyOneWord(){
+    words = new String[] {"listen", ""};
+    assertTrue(true, String.valueOf(anagram.isAnagram(words)));
+  }
+
+  @Test
+  void withOnlyOneWord2(){
+    words = new String[] {"listen", "silent"};
+    assertEquals(true, anagram.isAnagram(words));
+  }
+
+  @Test
+  void withEmptyList(){
+    words = new String[] {"", ""};
+    assertEquals(true, anagram.isAnagram(words));
+  }
+
+}
