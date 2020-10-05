aa2d087c023e4439bc113f227cfa71b1859e22f3
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-05 22:23:24 +0200

    (stream tasks) feat: added example 6

---
+++ week-13/src/Example6.java
@@ -0,0 +1,20 @@
+import java.util.stream.Collectors;
+
+public class Example6 {
+  public static void main(String[] args) {
+    //Write a Stream Expression to find the uppercase characters in a string!
+
+    String stringWithUpperCase = "Hi, I am software Developer";
+
+    String upperCaseFromString;
+
+    stringWithUpperCase.chars()
+        .filter(Character::isUpperCase)
+        .mapToObj(Character::toString)
+        .forEach(System.out::println);
+
+    //.collect(Collectors.joining());
+
+    // System.out.println(upperCaseFromString);
+  }
+}
