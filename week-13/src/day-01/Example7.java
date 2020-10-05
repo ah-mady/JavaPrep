97bd02e635667e56f757c3744d17dc2695cfc600
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-05 22:29:23 +0200

    added all classes in day-01

---
+++ week-13/src/day-01/Example7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Example7 {
+  public static void main(String[] args) {
+    //Write a Stream Expression to find the strings which starts with a given letter(as parameter), in the following list:
+
+    List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
+
+  }
+}
