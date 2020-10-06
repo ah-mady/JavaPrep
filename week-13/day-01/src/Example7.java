ea4f16cad2b983a309a8bdac14a5613d65eb3980
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-06 13:11:13 +0200

    (stream tasks) feat: added example 7

--- week-13/day-01/src/Example7.java
+++ week-13/day-01/src/Example7.java
@@ -4,8 +4,11 @@ public class Example7 {
   public static void main(String[] args) {
     //Write a Stream Expression to find the strings which starts with a given letter(as parameter), in the following list:
 
-    List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
+    List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM",
+            "ABU DHABI", "PARIS");
 
-
+    cities.stream()
+        .filter(city -> city.startsWith("A"))
+        .forEach(System.out::println);
   }
 }
