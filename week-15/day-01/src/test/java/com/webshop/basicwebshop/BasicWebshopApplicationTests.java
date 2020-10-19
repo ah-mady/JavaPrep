1960f27f80edf6001bf1f5f5bf1ee7c09895d8ac
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-19 19:39:05 +0200

    (WebShop) feat:  added shop item model class

---
+++ week-15/day-01/src/test/java/com/webshop/basicwebshop/BasicWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.basicwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebshopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
