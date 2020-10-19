934d8fa746cc7ecfa1e94a15c4b6025208a3bf3f
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-19 23:03:13 +0200

    (WebShop) feat:  added search bar and four functioning buttons

--- week-15/day-01/src/main/java/com/webshop/basicwebshop/controller/ShopItemController.java
+++ week-15/day-01/src/main/java/com/webshop/basicwebshop/controller/ShopItemController.java
@@ -2,10 +2,19 @@ package com.webshop.basicwebshop.controller;
 
 import com.webshop.basicwebshop.model.ShopItem;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShopItemController {
@@ -38,5 +47,71 @@ public class ShopItemController {
     model.addAttribute("items", shopItemList);
     return "homepage";
   }
+
+  @GetMapping("/only-available")
+  public String onlyAvailableItems(Model model) {
+    List<ShopItem> onlyItems = shopItemList.stream()
+        .filter(moreQuantityThanZero -> moreQuantityThanZero.getQuantityOfStock() > 0)
+        .collect(Collectors.toList());
+    model.addAttribute("items", onlyItems);
+    return "homepage";
+  }
+
+  @GetMapping("/cheapest-first")
+  public String cheapestFirst(Model model) {
+    List<ShopItem> sortedPrice = shopItemList.stream()
+        .sorted(Comparator.comparing(ShopItem::getPrice))
+        .collect(Collectors.toList());
+    model.addAttribute("items", sortedPrice);
+    return "homepage";
+  }
+
+  @GetMapping("/contains-nike")
+  public String containsNike(Model model) {
+    List<ShopItem> containNike = shopItemList.stream()
+        .filter(contains -> contains.getName() == "nike")
+        .collect(Collectors.toList());
+    model.addAttribute("items", containNike);
+    return "homepage";
+  }
+
+  @GetMapping("/average-stock")
+  public String averageStock(Model model) {
+    OptionalDouble averageStockPrice = shopItemList.stream()
+        .mapToDouble(i -> i.getQuantityOfStock())
+        .average();
+    model.addAttribute("averageStock", averageStockPrice);
+    return "stockAverage";
+  }
+
+  //  @GetMapping("/average-stock")
+//  public String averageStock(Model model){
+//    OptionalDouble averageStockPrice = shopItemList.stream()
+//        .mapToDouble(i -> i.getPrice())
+//        .average();
+//
+//    model.addAttribute("averageStock", averageStockPrice);
+//    return "stockAverage";
+//  }
+  @GetMapping("/most-expensive")
+  public String mostExpensive(Model model) {
+    String expensive = Collections.max(shopItemList, Comparator.comparing(ShopItem::getPrice)).getName();
+    model.addAttribute("expensive", expensive);
+    return "mostExpensive";
+  }
+
+  @PostMapping("/search")
+  public String searchResult(Model model, @RequestParam String shopItem){
+    Predicate<ShopItem> byName = e -> e.getName().toLowerCase().contains(shopItem.toLowerCase());
+    Predicate<ShopItem> byDescription = e -> e.getDescription().toLowerCase().contains(shopItem.toLowerCase());
+
+    List<ShopItem> searchedItem = shopItemList.stream()
+        .filter(byName.or(byDescription))
+        .collect(Collectors.toList());
+
+    model.addAttribute("items", searchedItem);
+    return "homepage";
+  }
+
 
 }
