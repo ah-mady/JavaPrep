e0f78fde35eeba8a52814670b88d3cfc320fa3d0
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-10-28 20:17:04 +0100

    (Dependency Inhection) feat: added task 1

---
+++ week-16/day-01/src/main/java/com/dependencyinjection/dependency/DependencyApplication.java
@@ -0,0 +1,26 @@
+package com.dependencyinjection.dependency;
+
+import com.dependencyinjection.dependency.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyApplication implements CommandLineRunner {
+
+  private Printer printer;
+
+  public DependencyApplication(Printer printer) {
+    this.printer = printer;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependencyApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("Hello, Dependency Injection");
+  }
+}
---
+++ week-16/day-01/src/test/java/com/dependencyinjection/dependency/DependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependencyinjection.dependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
