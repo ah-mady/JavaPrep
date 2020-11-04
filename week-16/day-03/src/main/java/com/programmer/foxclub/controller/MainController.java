5b80c1634b09975653b9111191d9833e412a0d81
Muhammad Ahmed Abdullah <ahmad_bhai28@yahoo.com>
2020-11-04 20:55:41 +0100

    (FoxClub) feat: added the nutrition getmapping page - postmapping, pending

--- week-16/day-03/src/main/java/com/programmer/foxclub/controller/MainController.java
+++ week-16/day-03/src/main/java/com/programmer/foxclub/controller/MainController.java
@@ -1,10 +1,16 @@
 package com.programmer.foxclub.controller;
 
+import com.programmer.foxclub.model.Drink;
+import com.programmer.foxclub.model.Food;
 import com.programmer.foxclub.model.Fox;
 import com.programmer.foxclub.service.FoxService;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
   private FoxService foxService;
+  private String currentlyLoggedFox;
 
+  @Autowired
   public MainController(FoxService foxService) {
     this.foxService = foxService;
   }
@@ -23,13 +31,8 @@ public class MainController {
       return "redirect:/login";
     }
     Fox myFox = this.foxService.getFoxName(name);
-    if (myFox != null) {
-      model.addAttribute("myFox", myFox);
-    } else {
-      model.addAttribute("name", name);
-      model.addAttribute("errormessage",
-          "You have provided a name that has not been used before, add it as a new one!");
-    }
+    model.addAttribute("myFox", myFox);
+    model.addAttribute("name", name);
     return "index";
   }
 
@@ -39,9 +42,32 @@ public class MainController {
   }
 
   @PostMapping("/login")
-  public String login(Model model, @RequestParam(required = false) String name) {
+  public String login(@RequestParam(required = false) String name) {
     foxService.addFox(name);
+    currentlyLoggedFox = name;
     return "redirect:/?name=" + name;
+  }
+
+  @GetMapping("/nutritionStore/")
+  public String nutritionStore(@RequestParam String name, Model model){
+    List<Food> foodList = Arrays.asList(Food.values());
+    List<Drink> drinkList = Arrays.asList(Drink.values());
+
+    model.addAttribute("name", foxService.getFoxName(name).getName());
+    model.addAttribute("foodList", foodList);
+    model.addAttribute("drinkList", drinkList);
+  return "nutritionStore";
+  }
+
+  @PostMapping("/nutritionStore")
+  public String updateNutrition(@RequestParam String name,
+                                @RequestParam Food food,
+                                @RequestParam Drink drink){
+    Fox myFox = foxService.getFoxName(name);
+    myFox.setFood(food);
+    myFox.setDrink(drink);
+
+    return "redirect:/" + name;
   }
 
 }
