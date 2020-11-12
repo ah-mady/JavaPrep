8c85837bcf0fd9f280b6ba96925a13ccf976cf7f
Ahmed <ahmad_bhai28@yahoo.com>
2020-11-12 20:04:51 +0100

    (ToDoWIthH2) feat: added todo controller with first method list()

---
+++ week-17/day-03/src/main/java/com/todowithh2/listing/controller/TodoController.java
@@ -0,0 +1,17 @@
+package com.todowithh2.listing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TodoController {
+
+  @ResponseBody
+  @RequestMapping(value={"/","/list"}, method= RequestMethod.GET)
+  public String list(){
+    return "This is my first ToDo";
+  }
+}
