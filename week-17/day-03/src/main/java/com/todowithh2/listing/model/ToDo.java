1a7d12e1ce1a05d640f4982b454c0337d881626a
Ahmed <ahmad_bhai28@yahoo.com>
2020-11-12 19:53:32 +0100

    (ToDoWIthH2) feat: added todo model class

---
+++ week-17/day-03/src/main/java/com/todowithh2/listing/model/ToDo.java
@@ -0,0 +1,47 @@
+package com.todowithh2.listing.model;
+
+public class ToDo {
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public ToDo(long id, String title) {
+    this.id = id;
+    this.title = title;
+    this.urgent = false;
+    this.done = false;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
