0415d321d6746bcc050ca6c6eb1ad578ed64d69f
Ahmed <ahmad_bhai28@yahoo.com>
2020-11-15 19:21:36 +0100

    (ConnectionWithMySQL) feat: added css that doesn't work

--- week-17/day-03/ConnectionWithMySql/src/main/java/com/connectionwith/mysql/controllers/TodoController.java
+++ week-17/day-03/ConnectionWithMySql/src/main/java/com/connectionwith/mysql/controllers/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,8 +47,15 @@ public class TodoController {
   @PostMapping("/add")
   public String addTodo(@ModelAttribute ToDo task){
     toDoRepository.save(task);
-
     return "redirect:/";
   }
+
+  @GetMapping("/{id}/delete")
+  public String deleteTask(@PathVariable("id") Long id, Model model){
+    model.addAttribute("id", id);
+    toDoRepository.deleteById(id);
+    return "redirect:/";
+  }
+
 
 }
